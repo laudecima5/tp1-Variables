63b4579dfb77de026b837088383e7dcd03a026f5
laudecima <laudecima0@gmail.com>
2025-06-12 16:39:53 -0300

    resolvi el ejercicio numero 5

---
+++ js/ejercicio5.js
@@ -0,0 +1,7 @@
+// 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+//Nota: Tener en cuenta la siguiente función: parseInt
+
+const num1 = parseInt(prompt('ingrese numero 1'))
+const num2 = parseInt(prompt('ingrese numero 2'))
+
+document.writeln('La suma del numero '+num1+' y el numero '+num2+' es: ' +(num1 + num2))
